89e443fdc70df94dadc7a9ce7a68e4555f60ed09
quybv <quybuivan@vccorp.vn>
2015-11-17 14:17:36 +0700

    Change ajax url

--- config/system_cfg.py
+++ config/system_cfg.py
@@ -1,6 +1,7 @@
 __author__ = 'quybvs'
 
-CHAPTER_URL = "http://www.vnthuquan.net/truyen/chuonghoi_moihon.aspx?&rand=860.9913737530052"
+CHAPTER_URL = "http://www.vnthuquan.net/truyen/chuonghoi_moi.aspx?&rand=860.9913737530052"
+# CHAPTER_URL = "http://www.vnthuquan.net/truyen/chuonghoi_moihon.aspx?&rand=860.9913737530052"
 VNTQ_COPYRIGHT_STRING = 'vnthuquan.net'
 # Define retry connect to server
 USER_AGENT = 'Mozilla/5.0'
